728ccbf70fa5ae6715275c0ce884377107d09f0a
shorux095-ctrl <shorux095@gmail.com>
2026-06-01 20:40:22 +0500

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "uz.daftar.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
