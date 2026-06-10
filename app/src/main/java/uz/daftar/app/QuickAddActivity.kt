b7ae5ececaced616120c5a6ee6a559f063e3a0c8
shorux095-ctrl <shorux095@gmail.com>
2026-06-10 22:47:09 +0500

    Add files via upload

--- app/src/main/java/uz/daftar/app/QuickAddActivity.kt
+++ app/src/main/java/uz/daftar/app/QuickAddActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
